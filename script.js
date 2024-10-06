ac222a4e0425a41661959a3332c7c9ba0dc364d3
zouhair <mizouhair4@gmail.com>
2024-10-06 15:01:50 +0200

    fix the back-ground::after

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const nav = document.getElementById('nav');  
-const Open = document.getElementById('open');  
-const Close = document.getElementById('close'); 
+const nav = document.getElementById("nav");
+const Open = document.getElementById("open");
+const Close = document.getElementById("close");
 
-Open.addEventListener('click', () => {
-    // Show the navigation and hide the open button
-    nav.style.display = "flex";
-    Open.style.display = "none";
-    Close.style.display = "block";
+Open.addEventListener("click", () => {
+  // Show the navigation and hide the open button
+  nav.style.display = "flex";
+  Open.style.display = "none";
+  Close.style.display = "block";
 });
 
-Close.addEventListener('click', () => {
-    // Hide the navigation and show the open button
-    nav.style.display = "none";
-    Close.style.display = "none";
-    Open.style.display = "block";
+Close.addEventListener("click", () => {
+  // Hide the navigation and show the open button
+  nav.style.display = "none";
+  Close.style.display = "none";
+  Open.style.display = "block";
 });
